090d8e53e99a78f3752c82add2ac6dc80a9d07d2
Ed rijo <rijoedi@gmail.com>
2023-11-12 23:51:50 +0900

    [WIP] change imageMagic to Gd lib

--- app/Http/Controllers/CheckingController.php
+++ app/Http/Controllers/CheckingController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\QrCodeModel;
+use App\Services\QrCodeService;
 
 
 class CheckingController extends Controller
 {
     public function generateQrCode()
     {
-        QrCodeModel::generateQrCode('email or phone number from client ??? ');
+        //TODO get the name of qr code from stripe webhook
+        QrCodeService::create('123', 'app/public/qrCodes/make_me_qrcode.png');
     }
 
 }
